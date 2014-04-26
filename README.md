78ceb457902ca5a377b268cb3701941a6db2d10c
Anubhav Joshi <anubhav9042@gmail.com>
2014-04-26 11:17:54 +0530

    Added README and SQL files.

---
+++ README.md
@@ -0,0 +1,17 @@
+IITP BLOOD BANK
+===============
+
+Objective of the project
+------------------------
+
+- To organize all the donors of our institute at one place.
+- To develop a portal where people can look out for other people who can help
+  them when in need of blood.
+
+How to use
+----------
+
+- You need a database like MySQL or any other.
+- Create a database by the name of `blood_bank`.
+- Import the `blood_bank.sql` provided.
+- Run `index.php`.
---
+++ blood_bank.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 26, 2014 at 05:46 AM
+-- Server version: 5.5.8
+-- PHP Version: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `blood_bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity_log`
+--
+
+CREATE TABLE IF NOT EXISTS `activity_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `activity` longtext NOT NULL,
+  `timelog` longtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `activity_log`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `records`
+--
+
+CREATE TABLE IF NOT EXISTS `records` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `user_hash` longtext NOT NULL,
+  `password` longtext NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `roll` varchar(30) NOT NULL,
+  `age` int(11) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `phone` bigint(40) NOT NULL,
+  `blood_group` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `records`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests`
+--
+
+CREATE TABLE IF NOT EXISTS `requests` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sender` int(11) NOT NULL,
+  `receipient` int(11) NOT NULL,
+  `response` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `requests`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suggestion`
+--
+
+CREATE TABLE IF NOT EXISTS `suggestion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `subject` longtext NOT NULL,
+  `suggestion` longtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `suggestion`
+--
+
